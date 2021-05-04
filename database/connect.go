209c1e6e8d16c123cd94790a056b3e936f497a00
CreamyMilk <creamym254@gmail.com>
2021-05-04 15:34:03 +0300

    📦 Database Connection can now be mapped to diffrent hosts and ports fix comment

--- database/connect.go
+++ database/connect.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 
-	//Used to provide postgress driver for database/sql package
+	//Used to provide mysql driver for database/sql package
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -14,7 +14,7 @@ var DB *sql.DB
 // Database settings
 const (
 	host     = "localhost"
-	port     = 5432 // Default port
+  port     = "3306" // Default port
 	user     = "root"
 	password = "test_pass"
 	dbname   = "agrodb"
@@ -24,7 +24,7 @@ const (
 func Connect() error {
 	var err error
 
-	DB, err = sql.Open("mysql", fmt.Sprintf("%s:%s@/%s", user, password, dbname))
+  DB, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host,port,dbname))
 	if err != nil {
 		return err
 	}
